fed889029b76f3d6b87fb9117669fd79ece0e70c
Cesar Mejia V <cesmejia8@gmail.com>
2022-05-15 02:27:55 -0500

    add throw error when saving changes to Core Data

--- CoreData-MVVM-Template/Persistence.swift
+++ CoreData-MVVM-Template/Persistence.swift
@@ -58,14 +58,15 @@ class PersistenceController: ObservableObject {
         let viewContext = container.viewContext
         
         if viewContext.hasChanges {
-            await viewContext.perform {
+            try await viewContext.perform {
                 do {
                     try viewContext.save()
                 } catch {
                     // Replace this implementation with code to handle the error appropriately.
                     // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                     let nsError = error as NSError
-                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+//                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                    throw nsError
                 }
             }
         }
